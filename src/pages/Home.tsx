bd00ea6a0a451ce79bddc7163f42dc206fcc0e40
ProgRies <cpries94@gmail.com>
2024-10-26 19:01:04 +0200

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Twitter, Copy, ExternalLink, Sword, Scroll } from 'lucide-react';
+import { ExternalLink, Sword, Scroll } from 'lucide-react';
 
 const Home = () => {
   return (
