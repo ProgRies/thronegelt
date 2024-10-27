c95109f0b73818adb1e8a8d66f57bba8d5ca632e
ProgRies <cpries94@gmail.com>
2024-10-27 11:17:50 +0100

    X icon removed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -44,7 +44,6 @@ const Home = () => {
               rel="noopener noreferrer"
               className="inline-flex items-center px-6 py-3 bg-blue-600 hover:bg-blue-500 rounded-lg text-white font-semibold transition-all"
             >
-              <X className="w-5 h-5 mr-2" />
               Follow @ThroneGelt
               <ExternalLink className="w-4 h-4 ml-2" />
             </a>
